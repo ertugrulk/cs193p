125a7e75b424bfc97615af3f8821d5dd945dd98d
ErtugrulK <athlon6000@gmail.com>
2020-11-01 16:45:33 +0100

    replaced emojiart project from tutorial

--- EmojiArt/EmojiArt/EmojiArt.swift
+++ EmojiArt/EmojiArt/EmojiArt.swift
@@ -2,16 +2,17 @@
 //  EmojiArt.swift
 //  EmojiArt
 //
-//  Created by Ertugrul Kara on 30/10/2020.
+//  Created by CS193p Instructor on 4/27/20.
+//  Copyright © 2020 Stanford University. All rights reserved.
 //
 
 import Foundation
 
-struct EmojiArt : Codable {
+struct EmojiArt: Codable {
     var backgroundURL: URL?
     var emojis = [Emoji]()
     
-    struct Emoji: Identifiable, Codable {
+    struct Emoji: Identifiable, Codable, Hashable {
         let text: String
         var x: Int
         var y: Int
@@ -27,12 +28,12 @@ struct EmojiArt : Codable {
         }
     }
     
-    var json: Data?{
+    var json: Data? {
         return try? JSONEncoder().encode(self)
     }
     
-    init?(json: Data?){
-        if json != nil, let newEmojiArt = try? JSONDecoder().decode(EmojiArt.self, from: json!){
+    init?(json: Data?) {
+        if json != nil, let newEmojiArt = try? JSONDecoder().decode(EmojiArt.self, from: json!) {
             self = newEmojiArt
         } else {
             return nil
@@ -40,9 +41,10 @@ struct EmojiArt : Codable {
     }
     
     init() { }
+    
     private var uniqueEmojiId = 0
     
-    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int){
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
         uniqueEmojiId += 1
         emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
     }
